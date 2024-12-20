554fb186877f4db64445a179ffe594dcb7d75280
RuKira <jduesling13@gmail.com>
2024-12-19 22:36:15 -0500

    Preparing for a rewrite, into my new system

---
+++ SPT-AllInOne/Windows/AvailForFinish.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows.Controls;
+
+namespace SPT_AllInOne.Pages;
+
+public partial class AvailForFinish : Page
+{
+    public AvailForFinish()
+    {
+        InitializeComponent();
+    }
+}
